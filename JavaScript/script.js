fd0f4b4ea8d168ceb189e829bc14411162dce8f1
Manny Verma <mannyverma11@gmail.com>
2025-01-07 22:23:32 -0700

    pushing faq code up

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,1 +1,10 @@
-const btns = document.querySelectorAll('.accordian');
+const btns = document.querySelectorAll('.heroTitle');
+
+btns.forEach((item) => {
+	const question = item.querySelector('question');
+	const answer = item.querySelector('answer');
+
+	question.addEventListener('click', () => {
+		answer.classList.toggle('hidden');
+	});
+});
