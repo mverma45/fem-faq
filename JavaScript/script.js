011d9aa65e67e5d62ff9e805dfc91faffdc3b1a9
Manny Verma <mannyverma11@gmail.com>
2025-01-17 14:48:00 -0700

    adding faq accordian

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,10 +1,12 @@
 //Selects all the btns for elements
-const btns = document.querySelectorAll('.question');
+const btns = document.querySelectorAll('.accordionItem');
 
-btns.forEach((question) => {
-	const pointer = question.querySelector('.pointerHero');
-	const answer = question.nextElementSibling;
+btns.forEach((accordion) => {
+	// const question = accordion.querySelector('.question');
+	const pointer = accordion.querySelector('.pointerHero');
+	const answer = accordion.querySelector('answer');
 
+	// if (pointer && answer) {
 	pointer.addEventListener('click', () => {
 		answer.classList.toggle('hidden');
 
@@ -16,5 +18,20 @@ btns.forEach((question) => {
 		} else {
 			icon.src = './assets/images/icon-minus.svg';
 		}
+
+		// Close open answers
+
+		question.forEach((otherAccordion) => {
+			if (otherAccordion !== accordion) {
+				const otherAnswer = otherAccordion.querySelector('.answer');
+				const otherIcon = otherAccordion.querySelector('.pointerHero img');
+
+				if (!otherAnswer.classList.contains('hidden')) {
+					otherAnswer.classList.add('hidden');
+					otherIcon.src = './assets/images/icon-plus.svg';
+				}
+			}
+		});
 	});
+	// }
 });
