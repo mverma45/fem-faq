5d94d0ace34a01fa383fbd26ea5d0cf65e292360
Manny Verma <mannyverma11@gmail.com>
2025-01-17 21:30:22 -0700

    fixed faq accordion

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,12 +1,11 @@
 //Selects all the btns for elements
-const btns = document.querySelectorAll('.accordionItem');
+const questions = document.querySelectorAll('.question');
 
-btns.forEach((accordion) => {
+questions.forEach((question) => {
 	// const question = accordion.querySelector('.question');
-	const pointer = accordion.querySelector('.pointerHero');
-	const answer = accordion.querySelector('answer');
+	const pointer = question.querySelector('.pointerHero');
+	const answer = question.nextElementSibling;
 
-	// if (pointer && answer) {
 	pointer.addEventListener('click', () => {
 		answer.classList.toggle('hidden');
 
@@ -18,20 +17,5 @@ btns.forEach((accordion) => {
 		} else {
 			icon.src = './assets/images/icon-minus.svg';
 		}
-
-		// Close open answers
-
-		question.forEach((otherAccordion) => {
-			if (otherAccordion !== accordion) {
-				const otherAnswer = otherAccordion.querySelector('.answer');
-				const otherIcon = otherAccordion.querySelector('.pointerHero img');
-
-				if (!otherAnswer.classList.contains('hidden')) {
-					otherAnswer.classList.add('hidden');
-					otherIcon.src = './assets/images/icon-plus.svg';
-				}
-			}
-		});
 	});
-	// }
 });
